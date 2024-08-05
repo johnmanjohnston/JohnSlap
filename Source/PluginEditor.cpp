0f24e140b2339ec1169abd31dd2fe7c2dcbfe0e8
johnmanjohnston <c.ethanjohn@gmail.com>
2024-08-05 19:24:53 +0400

    fretmarkers display for corresponding note

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -60,13 +60,14 @@ void JohnSlapAudioProcessorEditor::resized()
 
 void JohnSlapAudioProcessorEditor::changeListenerCallback(juce::ChangeBroadcaster* source)
 {
-    // TODO: look at kbListener.activeNotes and draw icons on the corresponding frets, maybe you'll need to call repaint()? 
-    DBG(kbListener.activeNotes.size());
+    // DBG(kbListener.activeNotes.size());
     repaint();
 }
 
 int* JohnSlapAudioProcessorEditor::getFretCoordinates(int noteNumber)
 {
+    static int retval[2] = { 0, 0 };
+    
     // at fret 5, the positions of strings starts to get a little off
     int stringPositions[4] = { 299, 292, 283, 277 };
 
@@ -74,6 +75,12 @@ int* JohnSlapAudioProcessorEditor::getFretCoordinates(int noteNumber)
     //                      OPEN   1    2    3    4    5    6   7
     int fretPositions[8] = { 263, 288, 320, 350, 377, 404, 428, 450 };
 
-    static int retval[2] = { 450, 299 };
+    // note number 28 is E1
+    int stringIndex = (noteNumber - 28) / 5;
+    int fretIndex = (noteNumber - 28) % 5;
+
+    retval[0] = fretPositions[fretIndex];
+    retval[1] = stringPositions[stringIndex];
+
     return retval;
 }
